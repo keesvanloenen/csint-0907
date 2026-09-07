25303d5350efd6ae99d550cf79a7bab0ed7ab621
Kees van Loenen <kees.vanloenen@infosupport.com>
2026-09-07 10:57:41 +0200

    Added nullable data types

---
+++ Verzekeringen/Verzekeringen.App/Klant.cs
@@ -0,0 +1,14 @@
+﻿namespace Verzekeringen.App;
+
+public class Klant
+{
+    public int Id { get; set; }                 // value type, standaard non-nullable
+    public string? Naam { get; set; }           // reference type, tegenwoordig ook standaard non-nullable
+    // public string Naam { get; set; } = null!;   // ik beloof dat ik nooit null wordt (bijv. constructor set Naam niet)
+
+    public Klant(string naam)
+    {
+        Naam = naam;
+    }
+}
+
---
+++ Verzekeringen/Verzekeringen.App/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Verzekeringen.App;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DemoNullableTypes();
+    }
+
+    private static void DemoNullableTypes()
+    {
+        decimal? premie = 20;
+        //Nullable<decimal> premie = null;
+
+        Console.WriteLine($"Premie is: {premie}");
+
+        Console.WriteLine(premie.HasValue ? premie.Value : "geen premie");
+
+        Klant klant1 = new Klant("An");
+        Console.WriteLine(klant1.Naam?.Length);
+    }
+}
