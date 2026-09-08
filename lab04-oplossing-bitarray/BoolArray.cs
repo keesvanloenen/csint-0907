38e623376c4bbaa76721d4abeb28412d773a245c
Kees van Loenen <kees.vanloenen@infosupport.com>
2026-09-08 11:41:35 +0200

    Oplossing lab04 bitarray toegevoegd

---
+++ lab04-oplossing-bitarray/BoolArray.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace BoolArrayProj;
+
+public class BoolArray
+{
+	private uint _size;
+	private ulong[] _data;
+
+	public BoolArray(uint size)
+	{
+		_size = size;
+		//_data = new ulong[(int)(size / 64 + Math.Ceiling(size % 64d))];
+		_data = new ulong[(size + 63) / 64];			// 👈 Compact way to calculate te number of ulong-elements to store 'size bits'
+                // eg. size = 130
+		// 130 / 64 = 2, but we'll need 3 ...
+		// (130 + 63) / 64 = 3
+	}
+
+    	public bool this[int index]
+	{
+		get
+		{
+			BoundaryCheck(index);
+
+			int dataIndex = index / 64;		// 👈 Which ulong ?
+			int bitIndex = index % 64;  		// 👈 Which bit (0-63) ?
+
+			return (_data[dataIndex] & (1UL << bitIndex)) != 0;
+
+			/* 🥸
+                         * _data[dataIndex] = 0b0001 (binary) => bitindex = 0
+                         * _data[dataIndex] = 0b1000 (binary) => bitindex = 3
+                         * assume: bitindex = 3
+
+			 * 1UL << 3 = 0b1000 (shift 1 left by 3 positions)
+			 * 0b1000 & 0b1000 (result is a non-zero)
+			 * 0b1000 != 0 -> true
+			*/
+        	}
+
+        	set
+		{
+            		BoundaryCheck(index);
+
+            		int dataIndex = index / 64; 		// 👈 Which ulong ?
+			int bitIndex = index % 64;  		// 👈 Which bit (0-63) ?
+			
+			if (value)
+			{
+				_data[dataIndex] |= (1UL << bitIndex);  // 👈 Set bit to 1
+
+				/* 🥸
+				 * Set bit 3 to 1 in the number 01010101
+				 * 1UL << 3 = 00001000
+				 * 01010101 |= 0001000 = 01011101 (bit 3 is now 1)
+				*/
+			}
+			else
+			{
+				_data[dataIndex] &= ~(1UL << bitIndex);	// 👈 Set bit to 0
+
+
+				/* 🥸
+				 * ~ => bitwise NOT: flips all bits (mask becomes all 1s except at bitindex)
+				 * Set bit 3 to 0 in the number 01011101
+				 * 1UL << 3 = 00001000
+				 * ~(1UL << 3) = 11110111
+				// 01011101 &= 11110111 = 01010101 (bit 3 is now 0)
+			}
+		}
+	}
+
+    private void BoundaryCheck(int index)
+    {
+        if (index < 0 || index >= _size)
+	{
+		throw new IndexOutOfRangeException();
+	}
+    }
+}
---
+++ lab04-oplossing-bitarray/BoolArrayTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BoolArrayProj.Tests;
+
+[TestClass]
+public class BoolArrayTests
+{
+    [TestMethod]
+    public void TestCreateArray()
+    {
+        // Arrange & Act
+        var array = new BoolArray(100);
+        
+        // Assert
+        Assert.IsNotNull(array);
+    }
+    
+    [TestMethod]
+    public void TestSetAndGet()
+    {
+        // Arrange
+        var array = new BoolArray(100);
+        
+        // Act & Assert
+        array[10] = true;
+        Assert.IsTrue(array[10]);
+        array[10] = false;
+        Assert.IsFalse(array[10]);
+    }
+    
+    [TestMethod]
+    public void TestMultipleSets()
+    {
+        // Arrange
+        var array = new BoolArray(100);
+        for (int i = 0; i < 100; i += 2)
+        {
+            array[i] = true;
+        }
+        
+        // Act & Assert
+        for (int i = 0; i < 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Assert.IsTrue(array[i]);
+            }
+            else
+            {
+                Assert.IsFalse(array[i]);
+            }
+        }
+    }
+    
+    [TestMethod]
+    public void TestIndexOutOfRange()
+    {
+        // Arrange
+        var array = new BoolArray(100);
+        
+        // Act & Assert
+        Assert.Throws<IndexOutOfRangeException>(() => { _ = array[100]; });
+    }
+
+    // Lab 4 Bonus:
+
+    [TestMethod]
+    public void TestCollectionInitializer()
+    {
+        // Arrange & Act
+        var array = new BoolArray(3)
+        {
+            [0] = true,
+            [1] = false,
+            [2] = false
+        };
+
+        // var array = new BoolArray(3) { true, false, true }; requires an Add(bool) method in BoolArray
+
+        // Assert
+        Assert.IsTrue(array[0]);
+        Assert.IsFalse(array[1]);
+        Assert.IsFalse(array[2]);
+        Assert.Throws<IndexOutOfRangeException>(() => array[4]);
+    }
+}
